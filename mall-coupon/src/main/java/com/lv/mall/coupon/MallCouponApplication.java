6c38fa92178947247fc8c7413564be9eea5e319f
2737550037@qq.com <2737550037@qq.com>
2022-10-17 11:27:12 +0800

    项目初始化

---
+++ mall-coupon/src/main/java/com/lv/mall/coupon/MallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.lv.mall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponApplication.class, args);
+    }
+
+}
